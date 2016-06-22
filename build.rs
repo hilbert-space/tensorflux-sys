9482627e041a18df42e6a356a0a9c9f8c3f63dca
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-22 13:31:45 +0200

    Compile TensorFlow

--- build.rs
+++ build.rs
@@ -14,12 +14,22 @@ fn main() {
     }
 
     let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("source");
+    let output = PathBuf::from(&get!("OUT_DIR"));
+
     if !Path::new(&source.join(".git")).exists() {
-        run("git", |command| command.args(&["clone", "--recurse-submodules", SOURCE_URL])
+        run("git", |command| command.arg("clone")
+                                    .arg("--recurse-submodules")
+                                    .arg(SOURCE_URL)
                                     .arg(&source));
     }
 
     run("./configure", |command| command.current_dir(&source));
+
+    run("bazel", |command| command.current_dir(&source)
+                                  .arg(format!("--output_base={}", output.display()))
+                                  .arg("build")
+                                  .arg("--compilation_mode=opt")
+                                  .arg("tensorflow:libtensorflow.so"));
 }
 
 fn run<F>(name: &str, mut configure: F) where F: FnMut(&mut Command) -> &mut Command {
