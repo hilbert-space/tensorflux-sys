0f437f1aac73899ce07a426a09c7d790109072f8
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-23 11:38:08 +0200

    Extend the example to run the session

--- examples/workflow.rs
+++ examples/workflow.rs
@@ -2,6 +2,7 @@ extern crate libc;
 extern crate tensorflow_sys as ffi;
 
 use libc::{c_int, c_longlong, c_void, size_t};
+use std::ffi::{CStr, CString};
 use std::path::Path;
 
 const GRAPH_PATH: &'static str = "tests/fixtures/graph.pb";
@@ -15,7 +16,11 @@ macro_rules! nonnull(
 );
 
 macro_rules! success(
-    ($status:expr) => (assert_eq!(ffi::TF_GetCode($status), ffi::TF_OK));
+    ($status:expr) => ({
+        if ffi::TF_GetCode($status) != ffi::TF_OK {
+            panic!(CStr::from_ptr(ffi::TF_Message($status)).to_string_lossy().into_owned());
+        }
+    });
 );
 
 fn main() {
@@ -28,13 +33,45 @@ fn main() {
         ffi::TF_ExtendGraph(session, graph.as_ptr() as *const _, graph.len() as size_t, status);
         success!(status);
 
-        let mut data = vec![69.0, 42.0];
-        let mut dims = vec![2 as c_longlong];
-        let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_DOUBLE, dims.as_mut_ptr(),
+        let mut input_names = vec![];
+        let mut inputs = vec![];
+
+        let name = CString::new("a:0").unwrap();
+        let mut data = vec![1.0f32, 2.0f32];
+        let mut dims = vec![data.len() as c_longlong];
+        let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_FLOAT, dims.as_mut_ptr(),
                                                 dims.len() as c_int, data.as_mut_ptr() as *mut _,
                                                 data.len() as size_t, Some(noop), 0 as *mut _));
 
-        ffi::TF_DeleteTensor(tensor);
+        input_names.push(name.as_ptr());
+        inputs.push(tensor);
+
+        let name = CString::new("b:0").unwrap();
+        let mut data = vec![3.0f32, 4.0f32];
+        let mut dims = vec![data.len() as c_longlong];
+        let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_FLOAT, dims.as_mut_ptr(),
+                                                dims.len() as c_int, data.as_mut_ptr() as *mut _,
+                                                data.len() as size_t, Some(noop), 0 as *mut _));
+
+        input_names.push(name.as_ptr());
+        inputs.push(tensor);
+
+        let mut output_names = vec![];
+        let mut outputs = vec![];
+
+        let name = CString::new("c:0").unwrap();
+
+        output_names.push(name.as_ptr());
+        outputs.push(0 as *mut _);
+
+        let mut target_names = vec![];
+
+        ffi::TF_Run(session, 0 as *const _, input_names.as_mut_ptr(), inputs.as_mut_ptr(),
+                    input_names.len() as c_int, output_names.as_mut_ptr(), outputs.as_mut_ptr(),
+                    output_names.len() as c_int, target_names.as_mut_ptr(),
+                    target_names.len() as c_int, 0 as *mut _, status);
+        success!(status);
+
         ffi::TF_DeleteSession(session, status);
         ffi::TF_DeleteStatus(status);
         ffi::TF_DeleteSessionOptions(options);
