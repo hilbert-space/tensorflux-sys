c0cee677d12eb2d7904ba782ef44444ed6a0edbd
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-24 06:46:10 +0200

    Compile in the root directory instead of the output one

--- build.rs
+++ build.rs
@@ -2,68 +2,60 @@ extern crate pkg_config;
 
 use std::path::{Path, PathBuf};
 use std::process::Command;
+use std::{env, fs};
 
-const SOURCE_URL: &'static str = "https://github.com/tensorflow/tensorflow.git";
+const FILENAME: &'static str = "libtensorflow.so";
+const LIBRARY: &'static str = "tensorflow";
+const REPOSITORY: &'static str = "https://github.com/tensorflow/tensorflow.git";
+const VERSION: &'static str = "0.9.0rc0";
 
-macro_rules! get(($name:expr) => (::std::env::var($name).unwrap()));
+macro_rules! get(($name:expr) => (env::var($name).unwrap()));
 
 fn main() {
-    if pkg_config::find_library("tensorflow").is_ok() {
+    if pkg_config::find_library(LIBRARY).is_ok() {
         return;
     }
 
     let output = PathBuf::from(&get!("OUT_DIR"));
+    if !output.join(FILENAME).exists() {
+        let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("source");
+        if !Path::new(&source.join(".git")).exists() {
+            run("git", |command| command.arg("clone")
+                                        .arg(format!("--branch=v{}", VERSION))
+                                        .arg("--recursive")
+                                        .arg(REPOSITORY)
+                                        .arg(&source));
+        }
 
-    if link(&output) {
-        return;
+        run("./configure", |command| command.current_dir(&source));
+        run("bazel", |command| command.current_dir(&source)
+                                      .arg("build")
+                                      .arg(format!("--jobs={}", get!("NUM_JOBS")))
+                                      .arg("--compilation_mode=opt")
+                                      .arg(format!("{}:{}", LIBRARY, FILENAME)));
+
+        fs::copy(find(&source, FILENAME).unwrap(), output.join(FILENAME)).unwrap();
     }
 
-    let source = PathBuf::from(&get!("CARGO_MANIFEST_DIR")).join("source");
-
-    if !Path::new(&source.join(".git")).exists() {
-        run("git", |command| command.arg("clone")
-                                    .arg("--branch=v0.9.0rc0")
-                                    .arg("--recurse-submodules")
-                                    .arg(SOURCE_URL)
-                                    .arg(&source));
-    }
-
-    run("./configure", |command| command.current_dir(&source));
-
-    run("bazel", |command| command.current_dir(&source)
-                                  .arg(format!("--output_base={}", output.display()))
-                                  .arg("build")
-                                  .arg(format!("--jobs={}", get!("NUM_JOBS")))
-                                  .arg("--compilation_mode=opt")
-                                  .arg("tensorflow:libtensorflow.so"));
-
-    assert!(link(&output));
+    println!("cargo:rustc-link-lib=dylib={}", LIBRARY);
+    println!("cargo:rustc-link-search={}", output.display());
 }
 
 fn find(directory: &Path, file: &str) -> Option<PathBuf> {
-    for entry in std::fs::read_dir(directory).unwrap() {
+    for entry in fs::read_dir(directory).unwrap() {
         let entry = entry.unwrap();
         let path = entry.path();
         if path.is_dir() {
             if let Some(path) = find(&path, file) {
                 return Some(path);
             }
-        } else if path.is_file() && path.ends_with(file) {
-            return Some(directory.into());
+        } else {
+            if path.is_file() && path.ends_with(file) {
+                return Some(path);
+            }
         }
     }
     None
-}
-
-fn link(directory: &Path) -> bool {
-    match find(directory, "libtensorflow.so") {
-        Some(directory) => {
-            println!("cargo:rustc-link-lib=dylib=tensorflow");
-            println!("cargo:rustc-link-search={}", directory.display());
-            true
-        },
-        _ => false,
-    }
 }
 
 fn run<F>(name: &str, mut configure: F) where F: FnMut(&mut Command) -> &mut Command {
