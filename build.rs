91966959d6667a4ec2cddc3c97a0ad117545a222
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-06-24 10:17:12 +0200

    Polish a bit the build script

--- build.rs
+++ build.rs
@@ -9,7 +9,8 @@ const LIBRARY: &'static str = "tensorflow";
 const REPOSITORY: &'static str = "https://github.com/tensorflow/tensorflow.git";
 const VERSION: &'static str = "0.9.0rc0";
 
-macro_rules! get(($name:expr) => (env::var($name).unwrap()));
+macro_rules! get(($name:expr) => (ok!(env::var($name))));
+macro_rules! ok(($expression:expr) => ($expression.unwrap()));
 
 fn main() {
     if pkg_config::find_library(LIBRARY).is_ok() {
@@ -34,7 +35,7 @@ fn main() {
                                       .arg("--compilation_mode=opt")
                                       .arg(format!("{}:{}", LIBRARY, FILENAME)));
 
-        fs::copy(find(&source, FILENAME).unwrap(), output.join(FILENAME)).unwrap();
+        ok!(fs::copy(ok!(find(&source, FILENAME)), output.join(FILENAME)));
     }
 
     println!("cargo:rustc-link-lib=dylib={}", LIBRARY);
@@ -42,8 +43,8 @@ fn main() {
 }
 
 fn find(directory: &Path, file: &str) -> Option<PathBuf> {
-    for entry in fs::read_dir(directory).unwrap() {
-        let path = entry.unwrap().path();
+    for entry in ok!(fs::read_dir(directory)) {
+        let path = ok!(entry).path();
         if path.is_dir() {
             if let Some(path) = find(&path, file) {
                 return Some(path);
@@ -59,7 +60,7 @@ fn find(directory: &Path, file: &str) -> Option<PathBuf> {
 
 fn run<F>(name: &str, mut configure: F) where F: FnMut(&mut Command) -> &mut Command {
     let mut command = Command::new(name);
-    if !configure(&mut command).status().unwrap().success() {
+    if !ok!(configure(&mut command).status()).success() {
         panic!("failed to execute {:?}", command);
     }
 }
