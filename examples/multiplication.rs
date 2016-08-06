56be46e16d02438ac2f30cb39abc23bf724cb5cd
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-06 15:44:27 +0200

    Rewrite the example using the new API

--- examples/multiplication.rs
+++ examples/multiplication.rs
@@ -1,9 +1,13 @@
 extern crate libc;
 extern crate tensorflux_sys as ffi;
 
-use libc::{c_int, c_void, int64_t, size_t};
-use std::ffi::{CStr, CString};
-use std::path::Path;
+fn main() {
+    unsafe { compute() };
+}
+
+macro_rules! cstr(
+    ($text:expr) => (CString::new($text).unwrap().as_ptr());
+);
 
 macro_rules! nonnull(
     ($pointer:expr) => ({
@@ -13,90 +17,91 @@ macro_rules! nonnull(
     });
 );
 
-macro_rules! ok(
-    ($status:expr) => ({
-        if ffi::TF_GetCode($status) != ffi::TF_OK {
-            panic!(CStr::from_ptr(ffi::TF_Message($status)).to_string_lossy().into_owned());
-        }
-    });
-);
-
-fn main() {
+unsafe fn compute() {
+    use libc::{c_int, c_void, int64_t, size_t};
+    use std::ffi::{CStr, CString};
     use std::mem::size_of;
     use std::ptr::{null, null_mut};
     use std::slice::from_raw_parts;
 
-    unsafe {
-        let options = nonnull!(ffi::TF_NewSessionOptions());
-        let status = nonnull!(ffi::TF_NewStatus());
-        let session = nonnull!(ffi::TF_NewSession(options, status));
+    let status = nonnull!(ffi::TF_NewStatus());
 
-        let graph = read("examples/assets/multiplication.pb"); // c = a * b
-        ffi::TF_ExtendGraph(session, graph.as_ptr() as *const _, graph.len() as size_t, status);
-        ok!(status);
+    macro_rules! ok(
+        ($result:expr) => ({
+            let result = $result;
+            if ffi::TF_GetCode(status) != ffi::TF_OK {
+                panic!(CStr::from_ptr(ffi::TF_Message(status)).to_string_lossy().into_owned());
+            }
+            result
+        });
+    );
 
-        let mut inputs = vec![];
-        let mut input_values = vec![];
+    let graph = nonnull!(ffi::TF_NewGraph());
 
-        let name = CString::new("a").unwrap();
-        let mut data = vec![1f32, 2.0, 3.0];
-        let dims = vec![data.len() as int64_t];
-        let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_FLOAT, dims.as_ptr(), dims.len() as c_int,
-                                                data.as_mut_ptr() as *mut _, data.len() as size_t,
-                                                Some(noop), null_mut()));
+    let description = nonnull!(ffi::TF_NewNode(graph, cstr!("Placeholder"), cstr!("a")));
+    ffi::TF_SetAttrType(description, cstr!("dtype"), ffi::TF_FLOAT);
+    let a = ok!(ffi::TF_FinishNode(description, status));
+    let a = ffi::TF_Port { node: a, index: 0 };
 
-        inputs.push(name.as_ptr());
-        input_values.push(tensor);
+    let description = nonnull!(ffi::TF_NewNode(graph, cstr!("Placeholder"), cstr!("b")));
+    ffi::TF_SetAttrType(description, cstr!("dtype"), ffi::TF_FLOAT);
+    let b = ok!(ffi::TF_FinishNode(description, status));
+    let b = ffi::TF_Port { node: b, index: 0 };
 
-        let name = CString::new("b").unwrap();
-        let mut data = vec![4f32, 5.0, 6.0];
-        let dims = vec![data.len() as int64_t];
-        let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_FLOAT, dims.as_ptr(), dims.len() as c_int,
-                                                data.as_mut_ptr() as *mut _, data.len() as size_t,
-                                                Some(noop), null_mut()));
+    let description = nonnull!(ffi::TF_NewNode(graph, cstr!("Mul"), cstr!("c")));
+    ffi::TF_AddInput(description, a);
+    ffi::TF_AddInput(description, b);
+    let c = ok!(ffi::TF_FinishNode(description, status));
+    let c = ffi::TF_Port { node: c, index: 0 };
 
-        inputs.push(name.as_ptr());
-        input_values.push(tensor);
+    let mut inputs = vec![];
+    let mut input_values = vec![];
 
-        let mut outputs = vec![];
-        let mut output_values = vec![];
+    let mut data = vec![1f32, 2.0, 3.0];
+    let dims = vec![data.len() as int64_t];
+    let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_FLOAT, dims.as_ptr(), dims.len() as c_int,
+                                            data.as_mut_ptr() as *mut _, data.len() as size_t,
+                                            Some(noop), null_mut()));
 
-        let name = CString::new("c").unwrap();
+    inputs.push(a);
+    input_values.push(tensor);
 
-        outputs.push(name.as_ptr());
-        output_values.push(null_mut());
+    let mut data = vec![4f32, 5.0, 6.0];
+    let dims = vec![data.len() as int64_t];
+    let tensor = nonnull!(ffi::TF_NewTensor(ffi::TF_FLOAT, dims.as_ptr(), dims.len() as c_int,
+                                            data.as_mut_ptr() as *mut _, data.len() as size_t,
+                                            Some(noop), null_mut()));
 
-        let mut targets = vec![];
+    inputs.push(b);
+    input_values.push(tensor);
 
-        ffi::TF_Run(session, null(), inputs.as_mut_ptr(), input_values.as_mut_ptr(),
-                    inputs.len() as c_int, outputs.as_mut_ptr(), output_values.as_mut_ptr(),
-                    outputs.len() as c_int, targets.as_mut_ptr(), targets.len() as c_int,
-                    null_mut(), status);
-        ok!(status);
+    let mut outputs = vec![];
+    let mut output_values = vec![];
 
-        let tensor = nonnull!(output_values[0]);
-        let data = nonnull!(ffi::TF_TensorData(tensor)) as *const f32;
-        let data = from_raw_parts(data, ffi::TF_TensorByteSize(tensor) / size_of::<f32>());
+    outputs.push(c);
+    output_values.push(null_mut());
 
-        assert_eq!(data, &[1.0 * 4.0, 2.0 * 5.0, 3.0 * 6.0]);
+    let targets = vec![];
 
-        ffi::TF_CloseSession(session, status);
+    let options = nonnull!(ffi::TF_NewSessionOptions());
+    let session = ok!(ffi::TF_NewSessionWithGraph(graph, options, status));
 
-        ffi::TF_DeleteTensor(tensor);
-        ffi::TF_DeleteSession(session, status);
-        ffi::TF_DeleteStatus(status);
-        ffi::TF_DeleteSessionOptions(options);
-    }
+    ok!(ffi::TF_SessionRun(session, null(), inputs.as_ptr(), input_values.as_ptr(),
+                           inputs.len() as c_int, outputs.as_ptr(), output_values.as_mut_ptr(),
+                           outputs.len() as c_int, targets.as_ptr(), targets.len() as c_int,
+                           null_mut(), status));
+
+    let tensor = nonnull!(output_values[0]);
+    let data = nonnull!(ffi::TF_TensorData(tensor)) as *const f32;
+    let data = from_raw_parts(data, ffi::TF_TensorByteSize(tensor) / size_of::<f32>());
+
+    assert_eq!(data, &[1.0 * 4.0, 2.0 * 5.0, 3.0 * 6.0]);
+
+    ok!(ffi::TF_CloseSessionWithGraph(session, status));
+    ok!(ffi::TF_DeleteSessionWithGraph(session, status));
+    ffi::TF_DeleteTensor(tensor);
+    ffi::TF_DeleteStatus(status);
+    ffi::TF_DeleteSessionOptions(options);
 
     unsafe extern "C" fn noop(_: *mut c_void, _: size_t, _: *mut c_void) {}
-}
-
-fn read<T: AsRef<Path>>(path: T) -> Vec<u8> {
-    use std::fs::File;
-    use std::io::Read;
-
-    let mut buffer = vec![];
-    let mut file = File::open(path).unwrap();
-    file.read_to_end(&mut buffer).unwrap();
-    buffer
 }
