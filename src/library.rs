5c3ce1d90886540144c08067bc4d7843783ac2f3
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-10-02 14:59:15 +0200

    Update the library API

--- src/library.rs
+++ src/library.rs
@@ -7,5 +7,7 @@ pub enum TF_Library {}
 
 extern {
     pub fn TF_LoadLibrary(name: *const c_char, status: *mut TF_Status) -> *mut TF_Library;
+    pub fn TF_DeleteLibraryHandle(library: *mut TF_Library);
     pub fn TF_GetOpList(library: *mut TF_Library) -> TF_Buffer;
+    pub fn TF_GetAllOpList() -> *mut TF_Buffer;
 }
